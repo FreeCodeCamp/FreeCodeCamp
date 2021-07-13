59dd6fcc4441c259393c8d711180c875324f8274
Igor Cunha <igorcesarcunha@gmail.com>
2021-07-13 11:35:14 -0400

    fix: refactor to ts and functional component

--- client/src/templates/Challenges/components/Preview.tsx
+++ client/src/templates/Challenges/components/Preview.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
+import React, { useState, useEffect } from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
 import { previewMounted } from '../redux';
@@ -10,7 +9,7 @@ import './preview.css';
 
 const mainId = 'fcc-main-frame';
 
-const mapDispatchToProps = dispatch =>
+const mapDispatchToProps = (dispatch: Dispatch) =>
   bindActionCreators(
     {
       previewMounted
@@ -18,49 +17,36 @@ const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-const propTypes = {
-  className: PropTypes.string,
-  disableIframe: PropTypes.bool,
-  previewMounted: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired
-};
+interface PreviewProps {
+  className?: string;
+  disableIframe?: boolean;
+  previewMounted: () => void;
+  t: (text: string) => string;
+}
 
-class Preview extends Component {
-  constructor(...props) {
-    super(...props);
+function Preview({ disableIframe, previewMounted, t }: PreviewProps) {
+  const [iframeStatus, setIframeStatus] = useState<boolean | undefined>(false);
+  const iframeToggle = iframeStatus ? 'disable' : 'enable';
 
-    this.state = {
-      iframeStatus: props.disableIframe
-    };
-  }
+  useEffect(() => {
+    previewMounted();
+  }, [previewMounted]);
 
-  componentDidMount() {
-    this.props.previewMounted();
-  }
+  useEffect(() => {
+    setIframeStatus(disableIframe);
+  }, [disableIframe]);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.disableIframe !== prevProps.disableIframe) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ iframeStatus: !this.state.iframeStatus });
-    }
-  }
-
-  render() {
-    const { t } = this.props;
-    const iframeToggle = this.state.iframeStatus ? 'disable' : 'enable';
-    return (
-      <div className={`notranslate challenge-preview ${iframeToggle}-iframe`}>
-        <iframe
-          className={'challenge-preview-frame'}
-          id={mainId}
-          title={t('learn.chal-preview')}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={`notranslate challenge-preview ${iframeToggle}-iframe`}>
+      <iframe
+        className={'challenge-preview-frame'}
+        id={mainId}
+        title={t('learn.chal-preview')}
+      />
+    </div>
+  );
 }
 
 Preview.displayName = 'Preview';
-Preview.propTypes = propTypes;
 
 export default connect(null, mapDispatchToProps)(withTranslation()(Preview));
