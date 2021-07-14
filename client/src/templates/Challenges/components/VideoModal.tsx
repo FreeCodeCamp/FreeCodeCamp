803116ec565d30302ab6f9f57020344f95a037ce
Igor Cunha <igorcesarcunha@gmail.com>
2021-07-14 10:07:15 -0400

    fix: migrate .js to .tsx, func compon implemented

--- client/src/templates/Challenges/components/VideoModal.tsx
+++ client/src/templates/Challenges/components/VideoModal.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import React from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { Modal } from '@freecodecamp/react-bootstrap';
 import { withTranslation } from 'react-i18next';
@@ -10,56 +9,54 @@ import { executeGA } from '../../../redux';
 
 import './video-modal.css';
 
-const mapStateToProps = state => ({ isOpen: isVideoModalOpenSelector(state) });
-const mapDispatchToProps = dispatch =>
+interface IVideoModalProps {
+  closeVideoModal: () => void;
+  executeGA: (attributes: { type: string; data: string }) => void;
+  isOpen?: boolean;
+  t: (attribute: string) => string;
+  videoUrl?: string;
+}
+
+const mapStateToProps = (state: unknown) => ({
+  isOpen: isVideoModalOpenSelector(state) as boolean
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
   bindActionCreators(
     { closeVideoModal: () => closeModal('video'), executeGA },
     dispatch
   );
 
-const propTypes = {
-  closeVideoModal: PropTypes.func.isRequired,
-  executeGA: PropTypes.func,
-  isOpen: PropTypes.bool,
-  t: PropTypes.func.isRequired,
-  videoUrl: PropTypes.string
-};
-
-export class VideoModal extends Component {
-  render() {
-    const { isOpen, closeVideoModal, videoUrl, executeGA, t } = this.props;
-    if (isOpen) {
-      executeGA({ type: 'modal', data: '/completion-modal' });
-    }
-    return (
-      <Modal
-        dialogClassName='video-modal'
-        onHide={closeVideoModal}
-        show={isOpen}
-      >
-        <Modal.Header
-          className='video-modal-header fcc-modal'
-          closeButton={true}
-        >
-          <Modal.Title className='text-center'>
-            {t('buttons.watch-video')}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='video-modal-body'>
-          <iframe
-            frameBorder='0'
-            src={videoUrl}
-            title={t('buttons.watch-video')}
-          />
-          <p>{t('learn.scrimba-tip')}</p>
-        </Modal.Body>
-      </Modal>
-    );
+export function VideoModal({
+  closeVideoModal,
+  executeGA,
+  isOpen,
+  t,
+  videoUrl
+}: IVideoModalProps): JSX.Element {
+  if (isOpen) {
+    executeGA({ type: 'modal', data: '/completion-modal' });
   }
+  return (
+    <Modal dialogClassName='video-modal' onHide={closeVideoModal} show={isOpen}>
+      <Modal.Header className='video-modal-header fcc-modal' closeButton={true}>
+        <Modal.Title className='text-center'>
+          {t('buttons.watch-video')}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='video-modal-body'>
+        <iframe
+          frameBorder='0'
+          src={videoUrl}
+          title={t('buttons.watch-video')}
+        />
+        <p>{t('learn.scrimba-tip')}</p>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 VideoModal.displayName = 'VideoModal';
-VideoModal.propTypes = propTypes;
 
 export default connect(
   mapStateToProps,
